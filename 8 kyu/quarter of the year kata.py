0d20f1d9560f76dfb5387ec8c93a04fdd0f6b1ce
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-25 11:03:14 +0200

    kata solution

---
+++ 8 kyu/quarter of the year kata.py
@@ -0,0 +1,13 @@
+#quarter of the year kata
+
+def quarter_of(month):
+    if month <= 3:
+        return 1
+    elif month <= 6:
+        return 2
+    elif month <= 9:
+        return 3
+    else:
+        return 4
+
+print(quarter_of(3))
