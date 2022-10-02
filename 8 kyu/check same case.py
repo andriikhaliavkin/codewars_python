be9fe6c0830cc93a81bd25279ae85f852f435727
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-10-02 19:45:20 +0200

    check same case kata solution

--- 8 kyu/check same case.py
+++ 8 kyu/check same case.py
@@ -3,3 +3,21 @@
 # If either of the characters is not a letter, return -1
 # If both characters are the same case, return 1
 # If both characters are letters, but not the same case, return 0
+
+
+def same_case(a, b):
+    if a.isalpha() and b.isalpha():
+        if (a.isupper() and b.isupper()) or (a.islower() and b.islower()):
+            return 1
+        else:
+            return 0
+    else:
+        return -1
+
+
+same_case('C', 'B')
+same_case('b', 'a')
+same_case('A', 's')
+same_case('c', 'B')
+same_case('\t', 'Z')
+same_case('H', ':')
